4601dba60ca958fbe76790b44d0d7d84c18803e5
Daria-Tsymbaliuk <dasha14072011@gmail.com>
2025-02-02 16:07:38 +0000

    Deploying to gh-pages from @ Daria-Tsymbaliuk/Ice-Cafe@ce6398612a1f09a247caee99f27b627dfe2f73b6 🚀

---
+++ index.1efb1cc6.js
@@ -0,0 +1,2 @@
+let e=1;!function(l){let t;let s=document.getElementsByClassName("mySlides"),a=document.getElementsByClassName("dot");for(1>s.length&&(e=1),t=0;t<s.length;t++)s[t].style.display="none";for(t=0;t<a.length;t++)a[t].className=a[t].className.replace(" active","");s[e-1].style.display="block",a[e-1].className+=" active"}(1);
+//# sourceMappingURL=index.1efb1cc6.js.map
