ae72f305952e93498479bc349c66465ea05de0ff
Katya-Semeniuk <109156601+Katya-Semeniuk@users.noreply.github.com>
2025-02-18 17:57:10 +0000

    Deploying to gh-pages from @ Daria-Tsymbaliuk/Ice-Cafe@2b8ac59ff367f429f8b7cd5db0eb0366182d335a 🚀

--- index.e41997f3.js
+++
@@ -1,2 +0,0 @@
-!function(){let e=1;function l(l){let t;let n=document.getElementsByClassName("mySlides"),s=document.getElementsByClassName("dot");for(l>n.length&&(e=1),l<1&&(e=n.length),t=0;t<n.length;t++)n[t].style.display="none";for(t=0;t<s.length;t++)s[t].className=s[t].className.replace(" active","");n[e-1].style.display="block",s[e-1].className+=" active"}l(1),window.plusSlides=function(t){l(e+=t)},window.currentSlide=function(t){l(e=t)}}();
-//# sourceMappingURL=index.e41997f3.js.map
