cc9c307f9542df69215de7a8b3fab5fc3c3ca395
Nierochron <real1zed993@gmail.com>
2025-02-21 16:04:50 +0100

    fix

--- src/js/products-modal.js
+++ src/js/products-modal.js
@@ -1,25 +1,36 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// Get the all buttons with class name roducts-btn
+const btnsOpen = document.querySelectorAll(".products-btn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the element that closes the modal
+const btnClose = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// Get the button submit form
+const btnsSubmit = document.getElementById("btnSubmit");
+
+// Додаємо обробник подій на кожну кнопку
+btnsOpen.forEach((btn) => {
+btn.addEventListener("click", () => {
+modal.style.display = "block"; // When the user clicks the button, open the modal
+});
+});
+
+// When the user clicks on (x), close the modal
+btnClose.onclick = function() {
+modal.style.display = "none";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// When the user clicks on submit button, close the modaal
+btnsSubmit.onclick = function() {
+modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+if (event.target == modal) {
+console.log(event.target)
+modal.style.display = "none";
+}
 }
